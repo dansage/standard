ac956e886ef2c9518993fc9b0abc7fef93c44b67
Daniel Sage <daniel@noreply.dsage.org>
2025-05-21 18:13:13 -0500

    build: add automatic version detection

---
+++ build/build.go
@@ -0,0 +1,79 @@
+package build
+
+import (
+	"runtime/debug"
+)
+
+// DetectedVersion indicates the VCS commit info when the project was compiled
+var DetectedVersion string
+
+// GoVersion indicates the version of Go used when the project was compiled
+var GoVersion string
+
+// staticVersion is the project version injected into the binary during the build process
+var staticVersion string
+
+// detectVersion uses the embedded build information to determine the version of the project. This automatically run
+// during initialization and should never be required.
+func detectVersion() {
+	// initialize the versions as "unknown"
+	DetectedVersion, GoVersion = "unknown", "unknown"
+
+	// read the build information from the binary
+	buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+		// notest
+		return
+	}
+
+	// store the Go version
+	GoVersion = buildInfo.GoVersion
+
+	// check if the static version was set during compilation
+	if staticVersion != "" {
+		// store the static version as the detected version
+		DetectedVersion = staticVersion
+		return
+	}
+
+	rev := "dev"
+	modified := false
+
+	// loop through the build settings to locate the VCS info
+	for _, setting := range buildInfo.Settings {
+		// notest
+		// justification:
+		// Go doesn't seem to store the VCS info in build info during testing, making this impossible for us to test as-is
+
+		// check if this is the revision
+		if setting.Key == "vcs.revision" {
+			// use the first 10 characters of the hash
+			rev = setting.Value[:10]
+		}
+
+		// check if this is the modified status
+		if setting.Key == "vcs.modified" {
+			modified = setting.Value == "true"
+		}
+	}
+
+	// store the detected version
+	if modified {
+		// notest
+		// justification:
+		// Go doesn't seem to store the VCS info in build info during testing, making this impossible for us to test as-is
+
+		DetectedVersion = rev + "-dirty"
+	} else {
+		// notest
+		// justification:
+		// Go doesn't seem to store the VCS info in build info during testing, making this impossible for us to test as-is
+
+		DetectedVersion = rev
+	}
+}
+
+func init() {
+	// detect the version automatically if required
+	detectVersion()
+}
---
+++ build/build_test.go
@@ -0,0 +1,40 @@
+package build
+
+import (
+	"runtime"
+	"testing"
+)
+
+// TestGoVersionIsDetected ensures the Go runtime version is automatically detected and is correct
+func TestGoVersionIsDetected(t *testing.T) {
+	// get the actual Go version from the runtime
+	ver := runtime.Version()
+
+	// verify the detected Go version matches the runtime version
+	if GoVersion != ver {
+		t.Fatalf("the detected Go version does not match: expected %q got %q", ver, GoVersion)
+	}
+}
+
+// TestStaticVersionIsDetected ensures the static version overrides the detection logic, if present
+func TestStaticVersionIsDetected(t *testing.T) {
+	// save the static version to restore it later
+	static := staticVersion
+
+	// set the static version to a marker value
+	staticVersion = "test-marker"
+
+	// re-detect the version
+	detectVersion()
+
+	// verify the detected version matches the marker value
+	if DetectedVersion != staticVersion {
+		t.Fatalf("the detected version does not match: expected %q got %q", staticVersion, DetectedVersion)
+	}
+
+	// restore the original static version value
+	staticVersion = static
+
+	// re-detect the version (to restore pre-test state)
+	detectVersion()
+}
